73c06a7364a220244140f332700ff09bf1a52f65
Rodrigo <rgonzalez@nectia.com>
2020-04-23 22:42:39 -0400

    Updates

---
+++ precache-manifest.a7d1769436df55b3cab29a20eb18cc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462674dcb50a8e0415e1e6798c23bf75",
+    "url": "/calculo-mental/index.html"
+  },
+  {
+    "revision": "bf1559d6efb29a5f5366",
+    "url": "/calculo-mental/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "efecc1549e1d0f3cc538",
+    "url": "/calculo-mental/static/js/2.c4594a35.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/calculo-mental/static/js/2.c4594a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1559d6efb29a5f5366",
+    "url": "/calculo-mental/static/js/main.79ff1c94.chunk.js"
+  },
+  {
+    "revision": "9177a2e0edc47a3eba76",
+    "url": "/calculo-mental/static/js/runtime-main.6e4a1228.js"
+  }
+]);
